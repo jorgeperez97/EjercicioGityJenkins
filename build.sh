8831128cc9e3d552f605d3759f74fee19c9a276f
jorgeperez97 <jorge.perez97@educa.madrid.org>
2024-11-15 17:11:52 +0100

    modificados:     build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #!/usr/bin/bash
 
 echo "Construyendo"
-wget https://www.w3schools.com/w3css/4/w3.css
 
+if [[ -e w3.css ]] ; then
+	echo "Ya existe el archivo"
+else
+	wget https://www.w3schools.com/w3css/4/w3.css
+fi
 if [[ $? -ne 0 ]] ; then 
  echo "Ha ocurrido un error. Se parará el script"
  exit 1
